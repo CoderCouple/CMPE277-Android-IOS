6ce77d9f8443977365e122cb697a7ae1ce1fa247
Suni28071987 <sunil28071987@gmail.com>
2017-09-15 21:47:50 -0700

    Guess The Number Application

--- GuesstheNumber/app/src/main/java/com/example/sunil28/guessthenumber/MainActivity.java
+++ GuesstheNumber/app/src/main/java/com/example/sunil28/guessthenumber/MainActivity.java
@@ -17,11 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
         if (number > 20)
             Toast.makeText(this, "Sorry! Number grater that 20. Enter Again.", Toast.LENGTH_SHORT).show();
-
-        if (number < 1)
+        else if (number < 1)
             Toast.makeText(this, "Sorry! Number less that 1. Enter Again.", Toast.LENGTH_SHORT).show();
-
-        if (number == secretNumber)
+        else if (number == secretNumber)
             Toast.makeText(this, "Hola! you got it...!!!", Toast.LENGTH_SHORT).show();
         else if (number < secretNumber)
             Toast.makeText(this, "higher !!", Toast.LENGTH_SHORT).show();
